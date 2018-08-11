e3f8e10a24a98871e3f632b151a0b9a6efd36038
sudeshnt <sudeshnt93@gmail.com>
2018-08-11 11:15:13 +0530

    media

--- src/pages/media/media.ts
+++ src/pages/media/media.ts
@@ -9,97 +9,67 @@ import { ModalController } from 'ionic-angular/components/modal/modal-controller
   templateUrl: 'media.html',
 })
 export class MediaPage {
-  data:any = {
-    items: [
-      {
-        id: 1,
-        image: 'assets/images/gallery/brogan/16.jpg'
-      },
-      {
-        id: 2,
-        image: 'assets/images/gallery/brogan/15.jpg'
-      },
-      {
-        id: 3,
-        image: 'assets/images/gallery/brogan/13.jpg'
-      },
-      {
-        id: 4,
-        image: 'assets/images/gallery/brogan/7.jpg'
-      },
-      {
-        id: 5,
-        image: 'assets/images/gallery/brogan/8.jpg'
-      },
-      {
-        id: 6,
-        image: 'assets/images/gallery/brogan/14.jpg'
-      },
-      {
-        id: 7,
-        image: 'assets/images/gallery/brogan/20.jpg'
-      },
-      {
-        id: 8,
-        image: 'assets/images/gallery/brogan/4.jpg'
-      },
-      {
-        id: 9,
-        image: 'assets/images/gallery/brogan/9.jpg'
-      },
-      {
-        id: 10,
-        image: 'assets/images/gallery/brogan/10.jpg'
-      },
-      {
-        id: 11,
-        image: 'assets/images/gallery/brogan/8.jpg'
-      },
-      {
-        id: 12,
-        image: 'assets/images/gallery/brogan/12.jpg'
-      },
-      {
-        id: 13,
-        image: 'assets/images/gallery/brogan/2.jpg'
-      },
-      {
-        id: 14,
-        image: 'assets/images/gallery/brogan/9.jpg'
-      },
-      {
-        id: 15,
-        image: 'assets/images/gallery/brogan/15.jpg'
-      },
-      {
-        id: 16,
-        image: 'assets/images/gallery/brogan/0.jpg'
-      }
-    ]
-  };
 
-  images = [{
-    url: `assets/images/gallery/brogan/0.jpg`
-  },{
-    url: `assets/images/gallery/brogan/0.jpg`
-  }];
+  images = [
+    {
+      url: 'assets/images/gallery/brogan/16.jpg'
+    },
+    {
+      url: 'assets/images/gallery/brogan/15.jpg'
+    },
+    {
+      url: 'assets/images/gallery/brogan/13.jpg'
+    },
+    {
+      url: 'assets/images/gallery/brogan/7.jpg'
+    },
+    {
+      url: 'assets/images/gallery/brogan/8.jpg'
+    },
+    {
+      url: 'assets/images/gallery/brogan/14.jpg'
+    },
+    {
+      url: 'assets/images/gallery/brogan/20.jpg'
+    },
+    {
+      url: 'assets/images/gallery/brogan/4.jpg'
+    },
+    {
+      url: 'assets/images/gallery/brogan/9.jpg'
+    },
+    {
+      url: 'assets/images/gallery/brogan/10.jpg'
+    },
+    {
+      url: 'assets/images/gallery/brogan/8.jpg'
+    },
+    {
+      url: 'assets/images/gallery/brogan/12.jpg'
+    },
+    {
+      url: 'assets/images/gallery/brogan/2.jpg'
+    },
+    {
+      url: 'assets/images/gallery/brogan/9.jpg'
+    },
+    {
+      url: 'assets/images/gallery/brogan/15.jpg'
+    },
+    {
+      url: 'assets/images/gallery/brogan/0.jpg'
+    }
+  ];
 
   constructor(public navCtrl: NavController, public navParams: NavParams, private modalCtrl: ModalController) {
   }
 
   ionViewDidLoad() {}
 
-  openImageSlider = (group: any, index: number): any => {
-    // this.navCtrl.push(group.fullscreen, {
-    //   'group': group.items,
-    //   'index': index
-    // });
-  }
-
-  openGallery() {
+  openGallery(initialSlide) {
     let modal = this.modalCtrl.create(GalleryModal, {
       photos: this.images,
-      initialSlide: 0,
+      initialSlide: initialSlide,
       closeIcon: 'back'
     });
     modal.present();
